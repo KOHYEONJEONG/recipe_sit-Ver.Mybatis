958e3b74a930b95c9d24ae38376c26e96eeb05cf
jhh0513 <jhh0513@onpia.co.kr>
2024-05-08 20:36:14 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/recipe/recipe_site/RecipeSiteApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.recipe_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSiteApplication.class, args);
+	}
+
+}
