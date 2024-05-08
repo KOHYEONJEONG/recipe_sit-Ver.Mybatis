ff6c55f080ab560bf0e6543e4f5dfaad6e571e86
jhh0513 <jhh0513@onpia.co.kr>
2024-05-08 20:44:18 +0900

    커밋 테스트

--- src/main/java/com/recipe/recipe_site/RecipeSiteApplication.java
+++ src/main/java/com/recipe/recipe_site/RecipeSiteApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeSiteApplication {
 
+	// 주석
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeSiteApplication.class, args);
 	}
