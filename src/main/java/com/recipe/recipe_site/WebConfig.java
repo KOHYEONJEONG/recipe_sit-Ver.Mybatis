ed127071f85e886f80344e3ee9fc93dbad514926
KOHYEONJEONG <080821@naver.com>
2024-06-16 23:52:18 +0900

    인터셉터 수정(아직 작성 안함)

--- src/main/java/com/recipe/recipe_site/WebConfig.java
+++ src/main/java/com/recipe/recipe_site/WebConfig.java
@@ -1,17 +1,42 @@
 package com.recipe.recipe_site;
 
+import com.recipe.recipe_site.grobal.interceptor.UserLocalInterceptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.Charset;
 import java.util.List;
 
+@Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final UserLocalInterceptor userLocaleInterceptor;
+
+    //인터셉터 등록
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        /**
+         * HTTP 요청 -> WAS -> 필터 -> 서블릿 -> 스프링 인터셉터 -> 컨트롤러 //로그인 사용자
+         * HTTP 요청 -> WAS -> 필터 -> 서블릿 -> 스프링 인터셉터(적절하지 않은 요청이라 판단, 컨트롤러 호출
+         * X) // 비 로그인 사용자
+         * */
+
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+    }
 
     /**
      * 메시지 소스를 UTF-8 인코딩으로 설정하는 예제입니다. 이를 통해 메시지 파일에서 한글과 같은
@@ -24,4 +49,8 @@ public class WebConfig implements WebMvcConfigurer {
         messageSource.setDefaultEncoding("UTF-8");//인코딩 설정
         return messageSource;
     }
+
+
+
+
 }
---
+++ src/main/java/com/recipe/recipe_site/grobal/interceptor/UserLocalInterceptor.java
@@ -0,0 +1,12 @@
+package com.recipe.recipe_site.grobal.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UserLocalInterceptor implements HandlerInterceptor {
+}
