fc8db40bc6e157c6661cd936ca805cb570bbf65e
KOHYEONJEONG <080821@naver.com>
2024-06-16 23:22:52 +0900

    UTF-8 다국어 인코딩 설정(인터셉터)

--- src/main/java/com/recipe/recipe_site/WebConfig.java
+++ src/main/java/com/recipe/recipe_site/WebConfig.java
@@ -13,20 +13,15 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        for (HttpMessageConverter<?> converter : converters) {
-            if (converter instanceof StringHttpMessageConverter) {
-                ((StringHttpMessageConverter) converter).setDefaultCharset(Charset.forName("UTF-8"));
-            }
-        }
-    }
-
+    /**
+     * 메시지 소스를 UTF-8 인코딩으로 설정하는 예제입니다. 이를 통해 메시지 파일에서 한글과 같은
+     * 다국어 문자열이 올바르게 읽히도록 할 수 있습니다.
+     * */
     @Bean
     public ReloadableResourceBundleMessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:messages");
-        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setBasename("classpath:messages");//scr/main/resources 디렉토리 아래에 messages.* 파일이 위치함을 의미함.
+        messageSource.setDefaultEncoding("UTF-8");//인코딩 설정
         return messageSource;
     }
 }
